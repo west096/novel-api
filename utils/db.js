effd3524dcbded399d6449fbd91cb498b939f37e
west096 <west096dev@gmail.com>
2025-08-06 01:12:49 +0900

    수정: Render DB 연결을 위한 SSL 설정 추가

--- utils/db.js
+++ utils/db.js
@@ -8,6 +8,7 @@ const pool = new Pool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
+  ssl: { rejectUnauthorized: false },
 });
 
 module.exports = pool;
